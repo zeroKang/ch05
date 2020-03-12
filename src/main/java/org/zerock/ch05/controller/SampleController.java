6ff976e09abc1605941b0f7361aeacb7bca749e9
zerock <cookieKang@gmail.com>
2020-03-12 22:20:42 +0900

    ex1( )

--- src/main/java/org/zerock/ch05/controller/SampleController.java
+++ src/main/java/org/zerock/ch05/controller/SampleController.java
@@ -1,4 +1,17 @@
 package org.zerock.ch05.controller;
 
+        import lombok.extern.log4j.Log4j2;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sample/*")
+@Log4j2
 public class SampleController {
+
+    @RequestMapping("/ex1")
+    public void ex1(){
+        log.info("ex1.............");
+    }
+
 }
